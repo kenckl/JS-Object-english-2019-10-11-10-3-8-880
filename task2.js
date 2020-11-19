f4fc698757e4e53deebf1d8d949177a2a6e7b92a
kenckl <44955521+kenckl@users.noreply.github.com>
2020-11-19 13:47:39 +0800

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,9 @@
+Program and observe the following requirements: calculate how many pieces of fruit there are in the fruit object and the output should be 50. 
+
+var fruit = {
+apple: 20,
+pear: 20,
+peach: 10
+};
+
+document.write(fruit.apple+fruit.pear+fruit.peach);
